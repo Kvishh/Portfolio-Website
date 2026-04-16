2c0cc5962fe4859ab9e4cd5630ad3766656404e1
Vish <kelvishvargas23@gmail.com>
2026-04-16 13:55:21 +0800

    Modified background of hero section and added images. Also added script

---
+++ script.js
@@ -0,0 +1,10 @@
+const bg_images = ["preview_2.png", "preview_4.png", "preview_6.png"]
+let index = 0
+
+function change_bg_image(){
+    index = (index + 1) % bg_images.length
+    document.querySelector("#background-image-holder").style.backgroundImage = `url(assets/images/${bg_images[index]})`;
+    console.log("hello")
+}
+
+setInterval(change_bg_image, 3000)
