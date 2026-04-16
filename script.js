b8202d29646a738f567ffb74529b5f5ba53509e9
Vish <kelvishvargas23@gmail.com>
2026-04-16 19:45:43 +0800

    Added a new font and more images. Finished half of hero section

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const bg_images = ["preview_2.png", "preview_4.png", "preview_6.png"]
+const bg_images = ["preview_2.png", "preview_3.png",  "preview_4.png", "preview_5.png", "preview_6.png", "preview_7.png"]
 let index = 0
 
 function change_bg_image(){
@@ -7,4 +7,4 @@ function change_bg_image(){
     console.log("hello")
 }
 
-setInterval(change_bg_image, 3000)
+setInterval(change_bg_image, 4000)
