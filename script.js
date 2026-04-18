cf8598ce3f8f92233575a30d1aee7ef64814bd44
Vish <kelvishvargas23@gmail.com>
2026-04-18 12:49:02 +0800

    Added certification section

--- script.js
+++ script.js
@@ -8,13 +8,35 @@ function change_bg_image(){
     document.querySelector("#background-image-holder").style.backgroundImage = `url(assets/images/${bg_images[index]})`;
 }
 
+const carouselContainer = document.querySelector(".carousel-container")
+carouselContainer.addEventListener("mousemove", (e)=>{
+    const rect = e.currentTarget.getBoundingClientRect();
+
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    carouselContainer.style.setProperty("--mouse-x", `${x}px`)
+    carouselContainer.style.setProperty("--mouse-y", `${y}px`)
+})
+
+const card = document.querySelector(".card")
+card.addEventListener("mousemove", (e)=>{
+    const rect = e.currentTarget.getBoundingClientRect();
+
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    card.style.setProperty("--mouse-x", `${x}px`)
+    card.style.setProperty("--mouse-y", `${y}px`)
+})
+
 setInterval(change_bg_image, 4000)
 
 gsap.to(".game-landing-section", {
     scrollTrigger: {
         trigger: ".game-landing-section",
         start: "bottom+=50px bottom",
-        end: "bottom top",
+        end: "bottom top-=70px",
         pin: true,
         pinSpacing: false
     },
